5f795f0108fdbe6648216fe408f6c700d908ad5c
Daniel <daniel@danielalemu.com>
2024-10-13 18:35:58 -0400

    Create new iOS project

---
+++ Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Daniel on 2024-10-13.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
