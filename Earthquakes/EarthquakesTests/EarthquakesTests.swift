59b8b09d8a05950058572cf2ae369e26390b1bc4
Daniel <daniel@danielalemu.com>
2024-10-15 19:04:30 -0400

    Adjust the date decoding strategy to make the test pass.

--- Earthquakes/EarthquakesTests/EarthquakesTests.swift
+++ Earthquakes/EarthquakesTests/EarthquakesTests.swift
@@ -12,6 +12,8 @@ final class EarthquakesTests: XCTestCase {
 
     func testGeoJSONDecoderDecodesQuake() throws {
         let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        
         let quake = try decoder.decode(Quake.self, from: testFeature_nc73649170)
         
         XCTAssertEqual(quake.code, "73649170")
@@ -19,6 +21,7 @@ final class EarthquakesTests: XCTestCase {
         let expectedSeconds = TimeInterval(1636129710550) / 1000 //From test data
         let decodedSeconds = quake.time.timeIntervalSince1970
         
+        //Validate the time of the quake
         XCTAssertEqual(expectedSeconds,
                        decodedSeconds,
                        accuracy: 0.00001)
