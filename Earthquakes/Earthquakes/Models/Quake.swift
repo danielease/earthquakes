074f259ee7a51cca2fc73746fe8240c5806d8716
Daniel <daniel@danielalemu.com>
2024-10-15 17:14:22 -0400

    Add Decodable conformance to Quake model

--- Earthquakes/Earthquakes/Models/Quake.swift
+++ Earthquakes/Earthquakes/Models/Quake.swift
@@ -17,3 +17,39 @@ struct Quake  {
 extension Quake: Identifiable {
     var id: String { code }
 }
+
+extension Quake: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case magnitude = "mag"
+        case place
+        case time
+        case code
+        case detail
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let magnitudeRaw = try? values.decode(Double.self, forKey: .magnitude)
+        let placeRaw = try? values.decode(String.self, forKey: .place)
+        let timeRaw = try? values.decode(Date.self, forKey: .time)
+        let codeRaw = try? values.decode(String.self, forKey: .code)
+        let detailRaw = try? values.decode(URL.self, forKey: .detail)
+        
+        guard let magnitude = magnitudeRaw,
+              let place = placeRaw,
+              let time = timeRaw,
+              let code = codeRaw,
+              let detail = detailRaw
+        else {
+            throw QuakeError.missingData
+        }
+        
+        self.magnitude = magnitude
+        self.place = place
+        self.time = time
+        self.code = code
+        self.detail = detail
+    }
+    
+}
