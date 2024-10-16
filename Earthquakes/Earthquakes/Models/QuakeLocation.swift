c2d9ca57cedf251740d47ec82e83d5aaed63bbf3
Daniel <daniel@danielalemu.com>
2024-10-15 23:49:51 -0400

    Declare intermediate structures leading to latitude and longitude
    - Make QuakeLocation conform to Decodable

--- Earthquakes/Earthquakes/Models/QuakeLocation.swift
+++ Earthquakes/Earthquakes/Models/QuakeLocation.swift
@@ -7,7 +7,27 @@
 
 import Foundation
 
-struct QuakeLocation {
+struct QuakeLocation: Decodable {
     var latitude: Double
     var longitude: Double
+    
+    struct RootProperties: Decodable {
+        var products: Products
+        
+    }
+    
+    struct Products: Decodable {
+        var origin: [Origin]
+    }
+    
+    struct Origin: Decodable {
+        var properties: OriginProperties
+    }
+    
+    struct OriginProperties: Decodable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    
 }
