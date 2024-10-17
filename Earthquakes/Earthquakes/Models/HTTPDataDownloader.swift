aa4ffa30b244abe37fd51d36727c73f562be1bcc
Daniel <daniel@danielalemu.com>
2024-10-16 22:19:54 -0400

    Use an URLSession extension to implement HTTPDataDownloader protocol method.

--- Earthquakes/Earthquakes/Models/HTTPDataDownloader.swift
+++ Earthquakes/Earthquakes/Models/HTTPDataDownloader.swift
@@ -7,6 +7,24 @@
 
 import Foundation
 
+let validHTTPStatus = 200...299
+
 protocol HTTPDataDownloader {
     func httpData(from: URL) async throws -> Data
+}
+
+//Use extension to implement protocol method for URLSession
+extension URLSession : HTTPDataDownloader {
+    func httpData(from url: URL) async throws -> Data {
+        guard let (data, response) = try await self.data(
+            from: url,
+            delegate: nil) as? (Data, HTTPURLResponse),
+              validHTTPStatus.contains(response.statusCode)
+        else {
+            throw QuakeError.networkError
+        }
+        
+        return data
+              
+    }
 }
