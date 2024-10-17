1857771c628bf5ca856fc91d7fc03a9ed711af9a
Daniel <daniel@danielalemu.com>
2024-10-17 00:22:45 -0400

    Create an observable object to serve as the view model for Quake views

---
+++ Earthquakes/Earthquakes/Models/QuakesProvider.swift
@@ -0,0 +1,14 @@
+//
+//  QuakesProvider.swift
+//  Earthquakes
+//
+//  Created by Daniel on 2024-10-17.
+//
+
+import Foundation
+
+class QuakesProvider : ObservableObject {
+    
+    @Published var quakes: [Quake] = []
+    
+}
