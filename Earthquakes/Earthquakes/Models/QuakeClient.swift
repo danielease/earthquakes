c11135a66730b3a7a3959d4ca3164bb356dc0ce9
Daniel <daniel@danielalemu.com>
2024-10-16 22:32:01 -0400

    Create class to represent quake client

---
+++ Earthquakes/Earthquakes/Models/QuakeClient.swift
@@ -0,0 +1,30 @@
+//
+//  QuakeClient.swift
+//  Earthquakes
+//
+//  Created by Daniel on 2024-10-16.
+//
+
+import Foundation
+
+class QuakeClient {
+    
+    //URL to fetch quake data from
+    private let quakeFeedURL = URL(string: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson")!
+    
+    //Custom JSON Decoder
+    private lazy var decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .millisecondsSince1970
+        return decoder
+    }()
+    
+    //Use an existential to define a downloader
+    private let downloader: any HTTPDataDownloader
+    
+    
+    init(downloader: any HTTPDataDownloader = URLSession.shared) {
+        self.downloader = downloader
+    }
+    
+}
